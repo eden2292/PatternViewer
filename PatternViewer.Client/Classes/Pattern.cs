2c4afdac08a4ef66fc4c87e04d091d909193ffd8
Eden Peacock <KP798@student.aru.ac.uk>
2024-08-03 21:23:45 +0100

    Navigation Menu added
    Added navigation menu, auto-populates from database entries.

---
+++ PatternViewer.Client/Classes/Pattern.cs
@@ -0,0 +1,44 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+
+namespace PatternViewer.Client.Classes
+{
+  public class Pattern
+  {
+    readonly SqlConnection connection = new("Data Source=DESKTOP-HQKTKN8;Initial Catalog=PatternViewer;Integrated Security=True;");
+    /// <summary>
+    /// create a data table of pattern names 
+    /// </summary>
+    /// <returns>Pattern names</returns>
+    public DataTable TryGetPatternNames()
+    {
+      DataTable dt = new DataTable();
+      using (connection)
+      {
+        string query = "SELECT PatternName FROM Pattern";
+        using (SqlDataAdapter adapter = new SqlDataAdapter(query, connection))
+        {
+          adapter.Fill(dt);
+        }
+      }
+      return dt;
+    }
+
+    /// <summary>
+    /// Create a List of pattern names from the data table
+    /// </summary>
+    /// <param name="dt">The data table</param>
+    /// <returns></returns>
+    public List<string> PatternNames(DataTable dt)
+    {
+      List<string> patternNames = new List<string>();
+      foreach (DataRow dr in dt.Rows)
+      {
+        string patternName = dr["PatternName"].ToString();
+        patternNames.Add(patternName);
+      }
+      return patternNames;
+    }
+
+  }
+}
